5162acc4db213e258bda946110cb5fd28ca6699f
John Steel <john@steelcomputers.com>
2015-11-11 12:52:09 -0500

    Initial Solution

---
+++ SeaSharpe_CVGS/SeaSharpe_CVGS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SeaSharpe_CVGS.Startup))]
+namespace SeaSharpe_CVGS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
