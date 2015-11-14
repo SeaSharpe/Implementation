f8f42209a58b52e85aca5bf734833fe6b39e5f9c
John Steel <john@steelcomputers.com>
2015-11-14 13:03:30 -0500

    Changed login to use username

--- SeaSharpe_CVGS/SeaSharpe_CVGS/Models/AccountViewModels.cs
+++ SeaSharpe_CVGS/SeaSharpe_CVGS/Models/AccountViewModels.cs
@@ -50,9 +50,8 @@ namespace SeaSharpe_CVGS.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "Username")]
+        public string Username { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
