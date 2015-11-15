2d8795a4d0aee4371aff875f639c4edf3095617c
Nicole Dahlquist <ndahlquist-cc@conestogac.on.ca>
2015-11-14 22:10:18 -0500

    ReviewController added

---
+++ SeaSharpe_CVGS/SeaSharpe_CVGS/Controllers/OrderController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SeaSharpe_CVGS.Models;
+
+namespace SeaSharpe_CVGS.Controllers
+{
+    public class OrderController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        #region Employee Side
+
+        /// <summary>
+        /// Employee Side - list all orders waiting to be processed
+        /// </summary>
+        /// <returns>Order Management view</returns>
+        public ActionResult OrderManagement()
+        {
+            return View(db.Orders.ToList());
+        }
+        #endregion
+        #region Member Side
+
+        /// <summary>
+        /// Member Side - list all order items for specified user
+        /// </summary>
+        /// <returns>Cart view</returns>
+        public ActionResult Cart()
+        {
+            return View(db.Orders.ToList());
+        }
+
+        #endregion
+       
+
+        // GET: /Order/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Order/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,OrderPlacementDate,ShipDate,IsProcessed")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Orders.Add(order);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(order);
+        }
+
+        // GET: /Order/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: /Order/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,OrderPlacementDate,ShipDate,IsProcessed")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(order);
+        }
+
+        // GET: /Order/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: /Order/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Order order = db.Orders.Find(id);
+            db.Orders.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SeaSharpe_CVGS/SeaSharpe_CVGS/Controllers/ReviewController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SeaSharpe_CVGS.Models;
+
+namespace SeaSharpe_CVGS.Controllers
+{
+    public class ReviewController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        #region Employee Side
+        /// <summary>
+        /// Employee Side - List all reviews awaiting review
+        /// </summary>
+        /// <returns>Review Management view</returns>
+        public ActionResult ReviewManagement()
+        {
+            return View(db.Reviews.ToList());
+        }
+
+        /// <summary>
+        /// post back for updating review to Accepted
+        /// **** No view required ****
+        /// </summary>
+        /// <param name="review">Review object</param>
+        /// <returns>Review Management view</returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Rating,Subject,Body")] Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(review).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("ReviewManagement");
+            }
+            return View(review);
+        }
+
+        /// <summary>
+        /// post back - review rejected by employee, delete review
+        /// **** no view required ****
+        /// </summary>
+        /// <param name="id">review id</param>
+        /// <returns>Review Management view</returns>
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Review review = db.Reviews.Find(id);
+            db.Reviews.Remove(review);
+            db.SaveChanges();
+            return RedirectToAction("ReviewManagement");
+        }
+
+        #endregion
+
+        #region Member Side
+        
+        /// <summary>
+        /// Member Side - List all reviews for a game and show average rating
+        /// </summary>
+        /// <param name="id">review id</param>
+        /// <returns>Reviews/Ratings view for specified game</returns>
+        public ActionResult ReviewsRating(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Review review = db.Reviews.Find(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+            return View(review);
+        }
+
+       /// <summary>
+        /// post back for review creation
+        /// **** review must be validated by employee before appears in Reviews/Rating list ****
+        /// </summary>
+       /// <param name="review">Review object</param>
+       /// <returns></returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Rating,Subject,Body")] Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Reviews.Add(review);
+                db.SaveChanges();
+                return RedirectToAction("Game/Details");
+            }
+
+            return View(review);
+        }
+        #endregion
+        
+        /// <summary>
+        /// garbage collection
+        /// </summary>
+        /// <param name="disposing">garbage</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
