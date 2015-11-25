4e328dc7aba6eb29220842133644e30bd89d3cd6
pjthomso <peter.thomson@live.ca>
2015-11-25 16:46:55 -0500

    Fixed the datePickerReady bug
    Because the datePickerReady bug makes a call to the jquery validator it
    had to be move into the jqueryval bundle, error is gone now and
    datepicker + validations appear to be working smoothly once again

--- SeaSharpe_CVGS/SeaSharpe_CVGS/App_Start/BundleConfig.cs
+++ SeaSharpe_CVGS/SeaSharpe_CVGS/App_Start/BundleConfig.cs
@@ -10,11 +10,11 @@ namespace SeaSharpe_CVGS
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/datePickerReady.js",
                         "~/Scripts/jquery-ui-1.11.4.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/datePickerReady.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
