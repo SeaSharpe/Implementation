85f267515c89da9d5bc099ab614d1749c9ba63f5
John Steel <john@steelcomputers.com>
2015-11-26 13:23:26 -0500

    Started to implement a custom controller class

---
+++ SeaSharpe_CVGS/SeaSharpe_CVGS/Controllers/Controller.cs
@@ -0,0 +1,26 @@
+﻿using SeaSharpe_CVGS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SeaSharpe_CVGS.Controllers
+{
+    public class Controller : System.Web.Mvc.Controller
+    {
+        ApplicationDbContext _db;
+
+        public ApplicationDbContext db
+        {
+            get
+            {
+                return _db ?? new ApplicationDbContext();
+            }
+
+            set
+            {
+                _db = value;
+            }
+        }
+    }
+}
