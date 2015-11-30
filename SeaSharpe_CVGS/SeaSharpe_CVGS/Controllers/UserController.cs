ad4723ee261ba8ea21dcc34501a4cdb96d102366
John Steel <john@steelcomputers.com>
2015-11-30 12:52:29 -0500

    Update profile now takes 3 models in the post
    Getting an error that the username can't be null when updating the
    profile...

--- SeaSharpe_CVGS/SeaSharpe_CVGS/Controllers/UserController.cs
+++ SeaSharpe_CVGS/SeaSharpe_CVGS/Controllers/UserController.cs
@@ -13,6 +13,8 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using SeaSharpe_CVGS.Models;
+using System.Data.Entity.Validation;
+using System.Text;
 
 namespace SeaSharpe_CVGS.Controllers
 {
@@ -27,16 +29,31 @@ namespace SeaSharpe_CVGS.Controllers
         /// <returns>Edit view</returns>
         public ActionResult Edit(int? id)
         {
-            Member member = db.Members.FirstOrDefault(m => m.Id == id || m.User.UserName == User.Identity.Name);
+            ProfileViewModel model = new ProfileViewModel
+            {
+                Member = db.Members.FirstOrDefault(m => m.Id == id || m.User.UserName == User.Identity.Name)
+            };
 
-            if (member == null)
+            var memberAddresses = db.
+                Addresses.
+                Where(a => a.Member.Id == model.Member.Id).
+                OrderBy(a => a.Id);
+
+            // Shipping address is the first address Billing adddress is the second address or a new adress
+            model.ShippingAddress = memberAddresses.FirstOrDefault() 
+                ?? new Address { Member = model.Member };
+            model.BillingAddress = memberAddresses.Skip(1).FirstOrDefault() 
+                ?? new Address { Member = model.Member };
+
+
+            if (model.Member == null)
             {
                 return HttpNotFound();
             }
 
-            if ( User.IsInRole("Employee") || member.User.UserName == User.Identity.Name )
+            if ( User.IsInRole("Employee") || model.Member.User.UserName == User.Identity.Name )
             {
-                return View(member);
+                return View(model);
             }
 
             throw new UnauthorizedAccessException("You may not access this profile.");
@@ -49,15 +66,80 @@ namespace SeaSharpe_CVGS.Controllers
         /// <returns>redirect to Game/SearchGames</returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include="Id,IsEmailVerified,IsEmailMarketingAllowed,StripeID")] Member member)
-        {
-            if (ModelState.IsValid)
+        public ActionResult Edit(
+            [Bind(Prefix = "Member")] Member member, 
+            [Bind(Prefix = "BillingAddress")] Address billingAddress, 
+            [Bind(Prefix = "ShippingAddress")] Address shippingAddress) {
+            var dbMember = db.Members.Find(member.Id);
+            var dbBillingAddress = db.Addresses.Find(billingAddress.Id);
+            var dbShippingAddress = db.Addresses.Find(billingAddress.Id);
+
+            if (dbMember == null)
             {
-                db.Entry(member).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Game/SearchGames");
+                TempData["Message"] = "Warning: Member not found.";
             }
-            return View(member);
+            else
+            {
+                UpdateMember(member, dbMember);
+            }
+
+            if (dbBillingAddress == null)
+            {
+                billingAddress.Member = member;
+                db.Addresses.Add(billingAddress);
+            }
+            else
+            {
+                UpdateAddress(billingAddress, dbBillingAddress);
+            }
+
+            if (dbShippingAddress == null)
+            {
+                shippingAddress.Member = member;
+                db.Addresses.Add(shippingAddress);
+            }
+            else
+            {
+                UpdateAddress(shippingAddress, dbShippingAddress);
+            }
+
+            if (ModelState.IsValid || true)
+            {
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbEntityValidationException e)
+                {
+                    StringBuilder sb = new StringBuilder("");
+                    foreach (var entity in e.EntityValidationErrors)
+                    {
+                        foreach (var error in entity.ValidationErrors)
+                        {
+                            sb.Append(string.Format("{0} -> {1}\n", error.PropertyName, error.ErrorMessage));
+                        }
+                    }
+                    TempData["Message"] = sb.ToString();
+                }
+                //return RedirectToAction("SearchGames", "Game");
+            }
+            return View(new ProfileViewModel { Member = member, BillingAddress = billingAddress, ShippingAddress = shippingAddress });
+        }
+
+        void UpdateMember(Member updateFrom, Member updateTo)
+        {
+            updateTo.User.FirstName = updateFrom.User.FirstName;
+            updateTo.User.LastName = updateFrom.User.LastName;
+            updateTo.User.Email = updateFrom.User.Email;
+            updateTo.User.Gender = updateFrom.User.Gender;
+            updateTo.IsEmailMarketingAllowed = updateFrom.IsEmailMarketingAllowed;
+        }
+
+        void UpdateAddress(Address updateFrom, Address updateTo)
+        {
+            updateTo.PostalCode = updateFrom.PostalCode;
+            updateTo.Region = updateFrom.PostalCode;
+            updateTo.StreetAddress= updateFrom.PostalCode;
         }
     }
 }
