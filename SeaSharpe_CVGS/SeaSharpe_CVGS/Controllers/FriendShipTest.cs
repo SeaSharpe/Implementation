f231471bbe29e6c2ed47636101a0211afcf191ea
Manuel Lopez <naaturaz@gmail.com>
2015-12-14 21:26:12 -0500

    Working on MoveToCart

--- SeaSharpe_CVGS/SeaSharpe_CVGS/Controllers/FriendShipTest.cs
+++ SeaSharpe_CVGS/SeaSharpe_CVGS/Controllers/FriendShipTest.cs
@@ -21,7 +21,12 @@ namespace SeaSharpe_CVGS.Controllers
                 //THOMASVALASCO422739 //BETHMILLARD721478 //ANNEMANNIX757533 //THOMASWILLIAMS156589
                 //BERNARDXAINTONG4274 //SARWATWINSMAN553131
 
-            AddFamily("KAYCHERNY183636");
+            //AddFamily("KAYCHERNY183636");
+            //Details(30000015);
+            //Delete(30000015);
+            //AddToWishList(7000001);
+            //RemoveFromWishlist(7000001);
+            MoveToCart(7000001);
         }
 
         //[SetUp]
@@ -50,14 +55,14 @@ namespace SeaSharpe_CVGS.Controllers
 
             Debug.Print(member.Id+" . "+member.User.UserName);
             // Act
-            controller.ControllerContext = GetControllerContext(db, member, new string[] {"Member"});
+            controller.ControllerContext = GetControllerContext(db, member, "Member");
             ViewResult result = controller.Index(searchName) as ViewResult;
 
             Assert.IsNotNull(result);
 
             if (result != null)
             {
-                Debug.Print("Edit() pass");
+                Debug.Print("Index() pass");
             }
         }
 
@@ -68,7 +73,7 @@ namespace SeaSharpe_CVGS.Controllers
             Debug.Print(controller.db.Members.Count() + "");
             Member member = controller.DbContext.Members.First();
 
-            controller.ControllerContext = GetControllerContext(db, member, new string[] { "Member" });
+            controller.ControllerContext = GetControllerContext(db, member, "Member");
 
             var initCount = controller.DbContext.Friendships.Where(a => a.Friender.User.Id == member.User.Id
                 && !a.IsFamilyMember).ToList().Count;
@@ -96,10 +101,9 @@ namespace SeaSharpe_CVGS.Controllers
         public void AddFamily(string userName)
         {
             var controller = new FriendshipController();
-            Debug.Print(controller.db.Members.Count() + "");
             Member member = controller.DbContext.Members.First();
-
-            controller.ControllerContext = GetControllerContext(db, member, new string[] { "Member" });
+            
+            controller.ControllerContext = GetControllerContext(db, member, "Member");
 
             var initCount = controller.DbContext.Friendships.Where(a => a.Friender.User.Id == member.User.Id
                 && a.IsFamilyMember).ToList().Count;
@@ -127,28 +131,130 @@ namespace SeaSharpe_CVGS.Controllers
         public void Details(int id)
         {
             var controller = new FriendshipController();
-            Debug.Print(controller.db.Members.Count() + "");
             Member member = controller.DbContext.Members.First();
 
-            controller.ControllerContext = GetControllerContext(db, member, new string[] { "Member" });
-
-            var initCount = controller.DbContext.Friendships.Where(a => a.Friender.User.Id == member.User.Id
-                && a.IsFamilyMember).ToList().Count;
+            controller.ControllerContext = GetControllerContext(db, member, "Member");
 
             // Act
             ViewResult result = controller.Details(id) as ViewResult;
 
-            var finCount = controller.DbContext.Friendships.Where(a => a.Friender.User.Id == member.User.Id
-                && a.IsFamilyMember).ToList().Count;
+            // Assert
+            Assert.IsNotNull(result);
+
+            if (result != null)
+            {
+                Debug.Print("Details() pass");
+            }
+        }
+
+
+        //[Test]
+        public void Delete(int id)
+        {
+            var controller = new FriendshipController();
+            Member member = controller.DbContext.Members.First();
+
+            controller.ControllerContext = GetControllerContext(db, member, "Member");
+
+            var initCount = controller.DbContext.Friendships.Where(a => a.Friender.User.Id == member.User.Id).ToList().Count;
+
+            Debug.Print(initCount + " before");
+
+            // Act
+            ViewResult result = controller.Delete(id) as ViewResult;
+
+            var finCount = controller.DbContext.Friendships.Where(a => a.Friender.User.Id == member.User.Id).ToList().Count;
+            Debug.Print(finCount + " after");
+
+            // Assert
+            Assert.AreEqual(initCount - 1, finCount);
+
+            if (initCount - 1 == finCount)
+            {
+                Debug.Print("Delete() pass");
+            }
+        }
+
+        void MoveToCart(int gameId)
+        {
+            var controller = new FriendshipController();
+            Member member = controller.DbContext.Members.First();
+
+            controller.ControllerContext = GetControllerContext(db, member, "Member");
+
+            var initCount = controller.DbContext.Orders.Where(a => a.Member.Id == member.Id).ToList().Count;
+            Debug.Print(initCount + " before");
+
+            // Act
+            ViewResult result = controller.MoveToCart(gameId, member.Id) as ViewResult;
+
+            var finCount = controller.DbContext.Orders.Where(a => a.Member.Id == member.Id).ToList().Count;
+            Debug.Print(finCount + " after");
 
             // Assert
             Assert.AreEqual(initCount + 1, finCount);
 
             if (initCount + 1 == finCount)
             {
-                Debug.Print("AddFamily() pass");
+                Debug.Print("MoveToCart() pass");
             }
         }
+
+
+        void RemoveFromWishlist(int id)
+        {
+            var controller = new FriendshipController();
+            Member member = controller.DbContext.Members.First();
+
+            controller.ControllerContext = GetControllerContext(db, member, "Member");
+
+            var initCount = controller.DbContext.WishLists.Where(w => w.MemberId == member.Id && w.GameId == id).ToList().Count;
+
+            Debug.Print(initCount + " before");
+
+            // Act
+            ViewResult result = controller.RemoveFromWishlist(id, member.Id) as ViewResult;
+
+            var finCount = controller.DbContext.WishLists.Where(w => w.MemberId == member.Id && w.GameId == id).ToList().Count;
+            Debug.Print(finCount + " after");
+
+            // Assert
+            Assert.AreEqual(initCount - 1, finCount);
+
+            if (initCount - 1 == finCount)
+            {
+                Debug.Print("RemoveFromWishlist() pass");
+            }
+        }
+
+        //[Test]
+        public void AddToWishList(int id)
+        {
+            var controller = new FriendshipController();
+            Member member = controller.DbContext.Members.First();
+
+            controller.ControllerContext = GetControllerContext(db, member, "Member");
+
+            var initCount = controller.DbContext.WishLists.Where(w => w.MemberId == member.Id && w.GameId == id).ToList().Count;
+
+            Debug.Print(initCount + " before");
+
+            // Act
+            ViewResult result = controller.AddToWishList(id) as ViewResult;
+
+            var finCount = controller.DbContext.WishLists.Where(w => w.MemberId == member.Id && w.GameId == id).ToList().Count;
+            Debug.Print(finCount + " after");
+
+            // Assert
+            Assert.AreEqual(initCount + 1, finCount);
+
+            if (initCount + 1 == finCount)
+            {
+                Debug.Print("AddToWishList() pass");
+            }
+        }
+
+
 
 
 
