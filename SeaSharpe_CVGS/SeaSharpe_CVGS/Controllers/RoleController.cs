041e638576fd95dd913b86e7fc6f4c99f89567d6
John Steel <john@steelcomputers.com>
2015-12-18 07:53:43 -0500

    documentation - Updated documentation comments and created Tech Doc - https://cvgs.duckdns.org/help/ will also display the docs

--- SeaSharpe_CVGS/SeaSharpe_CVGS/Controllers/RoleController.cs
+++ SeaSharpe_CVGS/SeaSharpe_CVGS/Controllers/RoleController.cs
@@ -7,6 +7,9 @@ using System.Web.Security;
 
 namespace SeaSharpe_CVGS.Controllers
 {
+    /// <summary>
+    /// This class is responsible for determining what users are in what roles
+    /// </summary>
     public class RoleController : RoleProvider
     {
         private ApplicationDbContext _dbContext;
@@ -29,11 +32,19 @@ namespace SeaSharpe_CVGS.Controllers
             None, Member, User, Employee
         }
 
+        /// <summary>
+        /// Not Implemented but override is required
+        /// </summary>
+        /// <param name="usernames"></param>
+        /// <param name="roleNames"></param>
         public override void AddUsersToRoles(string[] usernames, string[] roleNames)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Get the application name
+        /// </summary>
         public override string ApplicationName
         {
             get
@@ -46,26 +57,51 @@ namespace SeaSharpe_CVGS.Controllers
             }
         }
 
+        /// <summary>
+        /// Not Implemented but override is required
+        /// </summary>
+        /// <param name="roleName"></param>
         public override void CreateRole(string roleName)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Not Implemented but override is required
+        /// </summary>
+        /// <param name="roleName"></param>
+        /// <param name="throwOnPopulatedRole"></param>
+        /// <returns>NotImplementedException</returns>
         public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Not Implemented but override is required
+        /// </summary>
+        /// <param name="roleName"></param>
+        /// <param name="usernameToMatch"></param>
+        /// <returns>NotImplementedException</returns>
         public override string[] FindUsersInRole(string roleName, string usernameToMatch)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Get all roles
+        /// </summary>
+        /// <returns>A string array containing all rolenames</returns>
         public override string[] GetAllRoles()
         {
             return Enum.GetNames(typeof(Roles));
         }
 
+        /// <summary>
+        /// Get all roles for a user
+        /// </summary>
+        /// <param name="username">The user in question</param>
+        /// <returns>A string array containing all rolenames</returns>
         public override string[] GetRolesForUser(string username)
         {
             var roles = new System.Collections.Generic.List<string>();
@@ -78,6 +114,11 @@ namespace SeaSharpe_CVGS.Controllers
             return roles.ToArray();
         }
 
+        /// <summary>
+        /// Get all users for a role
+        /// </summary>
+        /// <param name="roleName">The name of the role</param>
+        /// <returns>A string array with all users in the role</returns>
         public override string[] GetUsersInRole(string roleName)
         {
             Roles role = Roles.None;
@@ -95,16 +136,32 @@ namespace SeaSharpe_CVGS.Controllers
             }
         }
 
+        /// <summary>
+        /// Find out if a user is in a role
+        /// </summary>
+        /// <param name="username">The username</param>
+        /// <param name="roleName">The role name</param>
+        /// <returns>true if the user is in the role</returns>
         public override bool IsUserInRole(string username, string roleName)
         {
             return GetUsersInRole(roleName).Contains(username);
         }
 
+        /// <summary>
+        /// Not Implemented but override is required
+        /// </summary>
+        /// <param name="usernames"></param>
+        /// <param name="roleNames"></param>
         public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Check if a role exists
+        /// </summary>
+        /// <param name="roleName">the role</param>
+        /// <returns>true when roleName does exist</returns>
         public override bool RoleExists(string roleName)
         {
             foreach (string role in Enum.GetNames(typeof(Roles)))
